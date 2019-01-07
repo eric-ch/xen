78e77039ec9ac11f4c6dadc0c7869fe9e8154861
Christopher Clark <christopher.w.clark@gmail.com>
2019-01-06 23:42:48 -0800

    argo: validate hypercall arg structures via compat machinery
    Argo doesn't use compat hypercall or argument translation but can use some
    of the infrastructure for validating the hypercall argument structures to
    ensure that the struct sizes, offsets and compositions don't vary between 32
    and 64bit, so add that here in a new dedicated source file for this purpose.
    
    Some of the argo hypercall argument structures contain elements that are
    hypercall argument structure types themselves, and the standard compat
    structure validation does not handle this, since the types differ in compat
    vs. non-compat versions; so for some of the tests the exact-type-match check
    is replaced with a weaker, but still sufficient, sizeof check.
    
    Then there are additional hypercall argument structures that contain
    elements that do not have a fixed size (last element, variable length array
    fields), so we have to then disable that size check too for validating those
    structures; the coverage of offset of elements is still retained.
    
    Signed-off-by: Christopher Clark <christopher.clark6@baesystems.com>

---
+++ xen/common/compat/argo.c
@@ -0,0 +1,61 @@
+/******************************************************************************
+ * Argo : Hypervisor-Mediated data eXchange
+ *
+ * Copyright (c) 2018, BAE Systems
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <xen/types.h>
+#include <xen/lib.h>
+#include <public/argo.h>
+#include <compat/argo.h>
+
+CHECK_argo_addr;
+CHECK_argo_register_ring;
+CHECK_argo_unregister_ring;
+
+/*
+ * Disable strict type checking in this compat validation macro for the
+ * following struct checks because it cannot handle fields within structs that
+ * have types that differ in the compat versus non-compat structs.
+ * Replace it with a field size check which is sufficient here.
+ */
+
+#undef CHECK_FIELD_COMMON_
+#define CHECK_FIELD_COMMON_(k, name, n, f) \
+static inline int __maybe_unused name(k xen_ ## n *x, k compat_ ## n *c) \
+{ \
+    BUILD_BUG_ON(offsetof(k xen_ ## n, f) != \
+                 offsetof(k compat_ ## n, f)); \
+    return sizeof(x->f) == sizeof(c->f); \
+}
+
+CHECK_argo_send_addr;
+CHECK_argo_ring_data_ent;
+CHECK_argo_iov;
+
+/*
+ * Disable sizeof type checking for the following struct checks because
+ * these structs have fields with variable size that the size check
+ * cannot validate.
+ */
+
+#undef CHECK_FIELD_COMMON_
+#define CHECK_FIELD_COMMON_(k, name, n, f) \
+static inline int __maybe_unused name(k xen_ ## n *x, k compat_ ## n *c) \
+{ \
+    BUILD_BUG_ON(offsetof(k xen_ ## n, f) != \
+                 offsetof(k compat_ ## n, f)); \
+    return 1; \
+}
+
+CHECK_argo_ring;
+CHECK_argo_ring_data;
