ec291dbe0894758b22e3dbee67d86090989a9820
Christopher Clark <christopher.w.clark@gmail.com>
2019-01-23 18:04:59 -0800

    xsm, argo: XSM control for argo register
    XSM controls for argo ring registration with two distinct cases, where
    the ring being registered is:
    
    1) Single source:  registering a ring for communication to receive messages
                       from a specified single other domain.
       Default policy: allow.
    
    2) Any source:     registering a ring for communication to receive messages
                       from any, or all, other domains (ie. wildcard).
       Default policy: deny, with runtime policy configuration via bootparam.
    
    This commit modifies the signature of core XSM hook functions in order to
    apply 'const' to arguments, needed in order for 'const' to be accepted in
    signature of functions that invoke them.
    
    Signed-off-by: Christopher Clark <christopher.clark6@baesystems.com>
    Acked-by: Daniel De Graaf <dgdegra@tycho.nsa.gov>
    
    v3 Daniel/Jan: add to the default xsm policy for the register op
    v3 hoist opt_argo_mac_permissive check to allow default policy to match non-XSM
    v3 was: Acked-by: Daniel De Graaf <dgdegra@tycho.nsa.gov>
    v3 Add Daniel's Acked-by ; note minor changes required for v4
    v3 feedback #07 Roger: use opt_argo_mac_permissive : a boolean opt
    v2 feedback #9 Jan: refactor to use argo-mac bootparam at point of introduction
    v1 feedback Paul: replace use of strncmp with strcmp
    v1 feedback #16 Jan: apply const to function signatures
    v1 feedback #14 Jan: add blank line before return in parse_argo_mac_param

--- xen/common/argo.c
+++ xen/common/argo.c
@@ -26,6 +26,7 @@
 #include <xen/nospec.h>
 #include <xen/sched.h>
 #include <xen/time.h>
+#include <xsm/xsm.h>
 
 #include <public/argo.h>
 
@@ -1677,8 +1678,10 @@ register_ring(struct domain *currd,
 
     if ( reg.partner_id == XEN_ARGO_DOMID_ANY )
     {
-        if ( !opt_argo_mac_permissive )
-            return -EPERM;
+        ret = opt_argo_mac_permissive ? xsm_argo_register_any_source(currd) :
+                                        -EPERM;
+        if ( ret )
+            return ret;
     }
     else
     {
@@ -1688,6 +1691,10 @@ register_ring(struct domain *currd,
             argo_dprintk("!dst_d, ESRCH\n");
             return -ESRCH;
         }
+
+        ret = xsm_argo_register_single_source(currd, dst_d);
+        if ( ret )
+            goto out;
 
         send_info = xzalloc(struct argo_send_info);
         if ( !send_info )
